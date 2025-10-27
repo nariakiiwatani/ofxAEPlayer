c0d70e57d83a6c0e57c5c426e37b4bcf6a0ed553
nariakiiwatani <nariakiiwatani@annolab.com>
2025-10-27 16:03:55 +0900

    retrieve multiply blendmode might be back for mask mode

--- src/utils/ofxAEBlendMode.cpp
+++ src/utils/ofxAEBlendMode.cpp
@@ -8,9 +8,7 @@ void applyBlendMode(BlendMode mode) {
 		case BlendMode::NORMAL:   ofEnableBlendMode(OF_BLENDMODE_ALPHA);   return;
 		case BlendMode::ADD:      ofEnableBlendMode(OF_BLENDMODE_ADD);     return;
 		case BlendMode::SUBTRACT: ofEnableBlendMode(OF_BLENDMODE_SUBTRACT);return;
-		case BlendMode::MULTIPLY:
-			glBlendFunc(GL_ZERO, GL_SRC_COLOR);
-			return;
+		case BlendMode::MULTIPLY: ofEnableBlendMode(OF_BLENDMODE_MULTIPLY); return;
 		case BlendMode::SCREEN:   ofEnableBlendMode(OF_BLENDMODE_SCREEN);  return;
 		case BlendMode::LIGHTEN:  ofEnableBlendMode(OF_BLENDMODE_MAX);     return;
 		case BlendMode::DARKEN:   ofEnableBlendMode(OF_BLENDMODE_MIN);     return;
